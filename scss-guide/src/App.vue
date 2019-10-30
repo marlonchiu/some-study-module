fd261b2002b840d6a7c94b0baaefe92a8c44001f
marlonchiu <Knight1898@outlook.com>
2019-10-30 15:54:52 +0800

    常用的mixin函数和全局样式使用

--- scss-guide/src/App.vue
+++ scss-guide/src/App.vue
@@ -1,56 +1,34 @@
 <template>
   <div>
-    <h2 class="container">定义变量</h2>
-    <div class="container1">
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
-    </div>
-    <h2>嵌套选择器</h2>
-    <div class="container2">
-      <ul>
-        <li><a>1</a></li>
-        <li><a>2</a></li>
-        <li><a>3</a></li>
-        <li><a>4</a></li>
-      </ul>
-    </div>
-    <h2 class="container3">导入SCSS文件</h2>
-    <h2 class="container3">
-      <p>导入SCSS文件说明</p>
-    </h2>
-    <div class="container4">
-      <ul>
-        <li><a>1</a></li>
-        <li><a>2</a></li>
-        <li><a>3</a></li>
-        <li><a>4</a></li>
-      </ul>
-    </div>
-    <h2>混合器</h2>
-    <div class="container5">
-      <ul>
-        <li><a>1</a></li>
-        <li><a>2</a></li>
-        <li><a>3</a></li>
-        <li><a>4</a></li>
-      </ul>
-    </div>
-    <h2>继承</h2>
-    <div class="container6">
-      <ul>
-        <li><a>1</a></li>
-        <li><a>2</a></li>
-        <li><a>3</a></li>
-        <li><a>4</a></li>
-      </ul>
+    <h3>Scss 中比较常用的mixin demo</h3>
+    <div class="container">
+
+      <p class="line-no-wrap">
+        今天的中国之所以取得如此巨大的发展成就，中国人民的生活之所以越来越富足，离不开这两个字：改革。在几十年的改革实践中，我们大胆地试、勇敢地改，
+        干出了一片新天地。尤其是十八大以来，以习近平同志为核心的党中央以巨大的政治勇气和智慧、更有力的措施和办法推进全面深化改革，取得了丰硕成果。
+      </p>
+      <p class="line-wrap-3">
+        今天的中国之所以取得如此巨大的发展成就，中国人民的生活之所以越来越富足，离不开这两个字：改革。在几十年的改革实践中，我们大胆地试、勇敢地改，
+        干出了一片新天地。尤其是十八大以来，以习近平同志为核心的党中央以巨大的政治勇气和智慧、更有力的措施和办法推进全面深化改革，取得了丰硕成果。
+        今天的中国之所以取得如此巨大的发展成就，中国人民的生活之所以越来越富足，离不开这两个字：改革。在几十年的改革实践中，我们大胆地试、勇敢地改，
+        干出了一片新天地。尤其是十八大以来，以习近平同志为核心的党中央以巨大的政治勇气和智慧、更有力的措施和办法推进全面深化改革，取得了丰硕成果。
+      </p>
+      <input type="text" placeholder="请输入内容">
+      <div class="box"></div>
+      <div class="pic-filter">
+        <img src="http://n.sinaimg.cn/news/1_img/upload/3ad618a7/192/w2048h1344/20190923/a706-ifaencf1466322.jpg" alt="img">
+      </div>
+      <p class="hover pointer click--invalid noselect">
+        鼠标hover显示下划线 | 点击显示小手 | 文本不允许选中 | 点击无效
+      </p>
+      <p class="ibass--default">
+        使用在字符图标上hover时的样式
+      </p>
     </div>
   </div>
 </template>
 
 <script></script>
 
-<style></style>
+<style lang='scss'>
+</style>
---
+++ scss-guide/src/App0.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2 class="container">定义变量</h2>
+    <div class="container1">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>
+    <h2>嵌套选择器</h2>
+    <div class="container2">
+      <ul>
+        <li><a>1</a></li>
+        <li><a>2</a></li>
+        <li><a>3</a></li>
+        <li><a>4</a></li>
+      </ul>
+    </div>
+    <h2 class="container3">导入SCSS文件</h2>
+    <h2 class="container3">
+      <p>导入SCSS文件说明</p>
+    </h2>
+    <div class="container4">
+      <ul>
+        <li><a>1</a></li>
+        <li><a>2</a></li>
+        <li><a>3</a></li>
+        <li><a>4</a></li>
+      </ul>
+    </div>
+    <h2>混合器</h2>
+    <div class="container5">
+      <ul>
+        <li><a>1</a></li>
+        <li><a>2</a></li>
+        <li><a>3</a></li>
+        <li><a>4</a></li>
+      </ul>
+    </div>
+    <h2>继承</h2>
+    <div class="container6">
+      <ul>
+        <li><a>1</a></li>
+        <li><a>2</a></li>
+        <li><a>3</a></li>
+        <li><a>4</a></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
