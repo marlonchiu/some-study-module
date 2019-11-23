ad42fb7ecfa051da6dbdebb98db61e68ee6db818
marlonchiu <Knight1898@outlook.com>
2019-11-23 16:00:06 +0800

    使用 remote 实现跨进程访问

--- electron-demo/renderer.js
+++ electron-demo/renderer.js
@@ -5,12 +5,17 @@
 // selectively enable features needed in the rendering
 // process.
 const { ipcRenderer } = require('electron')
+const { BrowserWindow } = require('electron').remote
+
 window.addEventListener('DOMContentLoaded', () => {
   document.getElementById('render-node').innerHTML = process.versions.node
   document.getElementById('send').addEventListener('click', () => {
     ipcRenderer.send('message', 'hello from renderer')
-    ipcRenderer.on('reply', (event, arg) => {
-      document.getElementById('reply-message').innerHTML = arg
-    })
+    // 打开一个新的窗口并访问百度
+    const newWin = new BrowserWindow({ width: 800, height: 600 })
+    newWin.loadURL('https://baidu.com')
+  })
+  ipcRenderer.on('reply', (event, arg) => {
+    document.getElementById('reply-message').innerHTML = arg
   })
 })
