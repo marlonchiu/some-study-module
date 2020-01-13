5955452c3439770249553558cb2fa3f2c07ba386
marlonchiu <Knight1898@outlook.com>
2020-01-13 15:49:14 +0800

    实现文件切片上传控制 项目初始化

---
+++ file-upload/README.md
@@ -0,0 +1,8 @@
+# file-upload
+
+从零搭建前端和服务端，实现一个大文件上传和断点续传的 demo
+
+## 写在前面
+
+> [字节跳动面试官：请你实现一个大文件上传和断点续传](https://juejin.im/post/5dff8a26e51d4558105420ed)
+> [GitHub 源码](https://github.com/yeyan1996/file-upload)
---
+++ file-upload/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <input type="file" @change="handleFileChange">
+    <el-button @click="handleUpload">上传</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: () => ({
+    container: {
+      file: null
+    }
+  }),
+  methods: {
+    async handleFileChange(e) {
+      const [file] = e.target.files
+      if (!file) return false
+      Object.assign(this.$data, this.$options.data())
+      this.container.file = file
+    },
+    async handleUpload() {}
+  }
+}
+</script>
+
+<style>
+</style>
