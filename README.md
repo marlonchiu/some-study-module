cd845adbf1b9aaa2de3cbf7d1f8082fb45f6f669
marlonchiu <Knight1898@outlook.com>
2020-01-15 13:25:22 +0800

    vue项目构建与开发入门

--- README.md
+++ README.md
@@ -27,4 +27,22 @@
 ## Zdog 3D动效实例
 
 > [made-with-zdog 对应的几个例子](https://zzz.dog/#made-with-zdog)
+> [Zdog-demo](https://github.com/marlonchiu/some-study-module/tree/master/Zdog-demo)
 
+## 手写webpack系列
+
+> [手写webpack系列 博文地址](https://juejin.im/post/5de87444518825124c50cd36)
+> 
+> [webpack-loader-plugin-guide](https://github.com/marlonchiu/some-study-module/tree/master/webpack-loader-plugin-guide)
+
+## file-upload 大文件上传和断点续传
+
+> [字节跳动面试官：请你实现一个大文件上传和断点续传](https://juejin.im/post/5dff8a26e51d4558105420ed)
+> 
+> [juejin-book-vue-build](https://github.com/marlonchiu/some-study-module/tree/master/file-upload)
+
+## Vue 项目构建与开发入门
+
+> [Vue 项目构建与开发入门](https://juejin.im/book/5b23a5aef265da59716fda09)
+> 
+> [juejin-book-vue-build](https://github.com/marlonchiu/some-study-module/tree/master/juejin-book-vue-build)
---
+++ juejin-book-vue-build/vue.config.js
@@ -0,0 +1,49 @@
+// 用于做相应的合并处理
+const merge = require('webpack-merge')
+// 项目的部署路径  前边表示项目启动时候的根路径地址
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/' : '/vue'
+
+module.exports = {
+  publicPath: BASE_URL,
+  // 修改打包文件输出文件夹 默认为 dist
+  outputDir: 'dist',
+  // 是否为生产环境构建生成 source map
+  productionSourceMap: true,
+  // chainWebpack 配置项允许我们更细粒度的控制 webpack 的内部配置
+  chainWebpack: config => {
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .tap(options => {
+        merge(options, {
+          limit: 5120
+        })
+      })
+  },
+  // configureWebpack 来进行修改，
+  // 两者的不同点在于 chainWebpack 是链式修改，
+  // 而 configureWebpack 更倾向于整体替换和修改
+  // config 参数为已经解析好的 webpack 配置
+  // 查看 plugins 的内容 ------ vue inspect plugins
+  configureWebpack: config => {
+    // config.plugins = [] // 这样会直接将 plugins 置空
+
+    // 使用 return 一个对象会通过 webpack-merge 进行合并，plugins 不会置空
+    return {
+      plugins: []
+    }
+  },
+  // 用于配置 webpack-dev-server 的行为，对本地服务器进行相应配置
+  devServer: {
+    open: true, // 是否自动打开浏览器页面 默认值false
+    host: '0.0.0.0', // 指定使用一个 host。默认是 localhost
+    port: 8090, // 端口地址 默认8080
+    https: false, // 使用https提供服务
+    proxy: null, // string | Object 代理设置
+
+    // 提供在服务器内部的其他中间件之前执行自定义中间件的能力
+    before: app => {
+      // `app` 是一个 express 实例
+    }
+  }
+}
